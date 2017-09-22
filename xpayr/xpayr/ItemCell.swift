d5525fcd8888144ea1f4816a4a4e7a6ef92bd663
Dunya Kirkali <dunya.kirkali@icemobile.com>
2017-09-22 22:31:49 +0200

    Visual tweaks

--- xpayr/xpayr/ItemCell.swift
+++ xpayr/xpayr/ItemCell.swift
@@ -14,6 +14,14 @@ class ItemCell: SwipeTableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var expirationLabel: UILabel!
     @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var container: UIView! {
+        didSet {
+            container.layer.shadowColor = UIColor.black.cgColor
+            container.layer.shadowOpacity = 0.2
+            container.layer.shadowOffset = CGSize.zero
+            container.layer.shadowRadius = 10
+        }
+    }
 
     var item: Item? {
         didSet {
